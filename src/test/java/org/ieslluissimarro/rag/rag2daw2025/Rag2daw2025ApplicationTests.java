cee1aca268199d473f1213f3be33aad1fa1f88e5
matecijas <matecijas@gmail.com>
2025-02-03 19:14:07 +0100

    Revert "prueba2"
    This reverts commit 5371baf199272fd2fa046d226fe6ffec4852c1f0.

---
+++ src/test/java/org/ieslluissimarro/rag/rag2daw2025/Rag2daw2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ieslluissimarro.rag.rag2daw2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rag2daw2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
