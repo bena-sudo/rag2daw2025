9824f1f936b289ee7e45c8b7f502d251ca9b1c1d
IvanTormo <156211078+IvanTormo@users.noreply.github.com>
2025-02-03 19:48:16 +0100

    Actualización documento SQL

--- BD_Documental.sql
+++ BD_Documental.sql
@@ -1,6 +1,7 @@
 CREATE TABLE documento (
     id SERIAL PRIMARY KEY,
-    dni VARCHAR(9) NOT NULL REFERENCES alumnos(dni) ON DELETE CASCADE,
+    idDocRag INT,
+    idUser SERIAL NOT NULL,
     nombre_fichero VARCHAR(255) NOT NULL,
     comentario TEXT,
     base64_documento TEXT,
@@ -12,19 +13,20 @@ CREATE TABLE documento (
 );
 
 CREATE TABLE etiqueta (
-    id SERIAL PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL UNIQUE
 );
 
 CREATE TABLE documento_etiqueta (
-    id_documento INT NOT NULL REFERENCES documento(id) ON DELETE CASCADE,
+    id_documento SERIAL NOT NULL REFERENCES documento(id) ON DELETE CASCADE,
     id_etiqueta INT NOT NULL REFERENCES etiqueta(id) ON DELETE CASCADE,
     PRIMARY KEY (id_documento, id_etiqueta)
 );
 
 CREATE TABLE DocumentChunk (
     id SERIAL PRIMARY KEY,
-    id_documento INT NOT NULL REFERENCES documento(id) ON DELETE CASCADE,
+    id_documento SERIAL NOT NULL REFERENCES documento(id) ON DELETE CASCADE,
+    idDocRag INT,
     chunk_order NUMERIC NOT NULL,
     chunk_text TEXT NOT NULL,
     chunked_by VARCHAR(255), -- Usuario que procesó el chunk
@@ -36,8 +38,8 @@ CREATE TABLE DocumentChunk (
 
 CREATE TABLE estadistica (
     id SERIAL PRIMARY KEY,
-    id_documento INT REFERENCES documento(id) ON DELETE SET NULL,
-    id_chunk INT REFERENCES DocumentChunk(id) ON DELETE SET NULL,
+    id_documento SERIAL REFERENCES documento(id) ON DELETE SET NULL,
+    id_chunk SERIAL REFERENCES DocumentChunk(id) ON DELETE SET NULL,
     tiempo_revision INT, -- Tiempo total en segundos
     fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     usuario VARCHAR(255), -- Usuario que realizó la revisión
