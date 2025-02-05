9d77ab9d68ecf25fbbd463caf79204560bcb6d2e
matecijas <matecijas@gmail.com>
2025-02-05 15:46:37 +0100

    feedbackservice

---
+++ src/main/java/org/ieslluissimarro/rag/rag2daw2025/srv/FeedbackService.java
@@ -0,0 +1,11 @@
+package org.ieslluissimarro.rag.rag2daw2025.srv;
+
+import org.ieslluissimarro.rag.rag2daw2025.model.dto.FeedbackEdit;
+
+public interface FeedbackService {
+
+    public FeedbackEdit create(FeedbackEdit ae);
+    public FeedbackEdit read(Long id);
+    public FeedbackEdit update(Long id, FeedbackEdit feedbackEdit);
+    public void delete(Long id);
+}
