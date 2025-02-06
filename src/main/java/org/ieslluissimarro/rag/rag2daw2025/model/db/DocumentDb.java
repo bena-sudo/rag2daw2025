d9bbcec0290572d6bcb526c8da787b7264f5d386
Laura11mp <laura_mico_perez@hotmail.com>
2025-02-06 16:22:08 +0100

    commit validació document

--- src/main/java/org/ieslluissimarro/rag/rag2daw2025/model/db/DocumentDb.java
+++ src/main/java/org/ieslluissimarro/rag/rag2daw2025/model/db/DocumentDb.java
@@ -2,12 +2,18 @@ package org.ieslluissimarro.rag.rag2daw2025.model.db;
 
 import java.time.LocalDateTime;
 
+import org.ieslluissimarro.rag.rag2daw2025.model.enums.EstadoDocumento;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,48 +25,51 @@ import lombok.NoArgsConstructor;
 
 @Table(name = "documentos")
 public class DocumentDb {
-  
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private Long id;
 
-   @Column(name = "id_doc_rag")
-   private Integer idDocRag;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-   // @Column(name = "dni", nullable = false)
-   // private String dni;
+    @Column(name = "id_doc_rag")
+    private Integer idDocRag;
 
+    // @Column(name = "dni", nullable = false)
+    // private String dni;
 
-   @Column(name = "nombre_fichero", nullable = false)
-   private String nombreFichero;
+    @NotNull(message = "El id del usuario no puede estar vacío")
+    @Column(name = "id_usuario", nullable = false)
+    private Long idUsuario;
 
+    @NotNull
+    @Size(max = 255, message = "El nombre debe tener un tamaño máximo de 255 caracteres")
+    @Column(name = "nombre_fichero", nullable = false, length = 255)
+    private String nombreFichero;
 
-   @Column(name = "comentario")
-   private String comentario;
+    @Column(name = "comentario")
+    private String comentario;
 
+    @NotNull(message = "El documento no puede estar vacío")
+    @Column(name = "base64_documento", nullable = false, columnDefinition = "TEXT")
+    private String base64Documento;
 
-   @Column(name = "base64_documento")
-   private String base64Documento;
+    @Size(max = 5, message = "La extensión del documento debe tener como máximo 5 caracteres")
+    @Column(name = "extension_documento")
+    private String extensionDocumento;
 
+    @Size(max = 50, message = "El tipo de documento debe tener como máximo 50 caracteres")
+    @Column(name = "content_type_documento")
+    private String contentTypeDocumento;
 
-   @Column(name = "extension_documento")
-   private String extensionDocumento;
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, columnDefinition = "VARCHAR(20) DEFAULT 'pendiente'")
+    private EstadoDocumento estadoDocumento;
 
+    @NotNull
+    @Column(name = "fecha_creacion", nullable = false)
+    private LocalDateTime fechaCreacion;
 
-   @Column(name = "content_type_documento")
-   private String contentTypeDocumento;
-
-
-   @Column(name = "estado_documento", nullable = false)
-   private String estadoDocumento = "pendiente";
-
-
-   @Column(name = "fecha_creacion", nullable = false)
-   private LocalDateTime fechaCreacion;
-
-   @Column(name = "fecha_revision")
-   private LocalDateTime fechaRevision;
-
+    @NotNull
+    @Column(name = "fecha_revision")
+    private LocalDateTime fechaRevision;
 
 }
-
--- src/main/java/org/ieslluissimarro/rag/rag2daw2025/model/dto/DocumentEdit.java
+++ src/main/java/org/ieslluissimarro/rag/rag2daw2025/model/dto/DocumentEdit.java
@@ -2,6 +2,15 @@ package org.ieslluissimarro.rag.rag2daw2025.model.dto;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import org.ieslluissimarro.rag.rag2daw2025.model.enums.EstadoDocumento;
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 
 @Data
@@ -9,13 +18,32 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class DocumentEdit {
 
-   private Long id;
-   private Integer idDocRag;
-   private String comentario;
-   private String estadoDocumento;
-   private String nombreFichero;
-   private String extensionDocumento;
-   private String contentTypeDocumento;
+    private Long id;
+
+    private Integer idDocRag;
+
+    @NotNull(message = "El id del usuario no puede estar vacío")
+    private Long idUsuario;
+
+    private String comentario;
+
+    @Enumerated(EnumType.STRING)
+    private EstadoDocumento estadoDocumento;
+
+    @NotNull
+    @Size(max = 255, message = "El nombre debe tener un tamaño máximo de 255 caracteres")
+    @Column(name = "nombre_fichero", nullable = false, length = 255)
+    private String nombreFichero;
+
+    // Campo para el archivo (no se persiste directamente en la base de datos. Debe pasarse a base64)
+    private MultipartFile multipart;
+
+    @Size(max = 5, message = "La extensión del documento debe tener como máximo 5 caracteres")
+    @Column(name = "extension_documento")
+    private String extensionDocumento;
+
+    @Size(max = 50, message = "El tipo de documento debe tener como máximo 50 caracteres")
+    @Column(name = "content_type_documento")
+    private String contentTypeDocumento;
 
 }
-
