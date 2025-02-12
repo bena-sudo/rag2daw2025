a31754bc53c126882984e83021acddd637d1c342
matecijas <matecijas@gmail.com>
2025-02-12 19:19:04 +0100

    filtros con paginables

--- src/main/java/org/ieslluissimarro/rag/rag2daw2025/controller/FiltrosYEstadisticasController.java
+++ src/main/java/org/ieslluissimarro/rag/rag2daw2025/controller/FiltrosYEstadisticasController.java
@@ -1,12 +1,20 @@
 package org.ieslluissimarro.rag.rag2daw2025.controller;
 
+import java.util.List;
 import java.util.Map;
 
+import org.ieslluissimarro.rag.rag2daw2025.helper.FiltroBusqueda;
+import org.ieslluissimarro.rag.rag2daw2025.helper.FiltroBusquedaHelper;
 import org.ieslluissimarro.rag.rag2daw2025.helper.JsonToMapHelper;
+import org.ieslluissimarro.rag.rag2daw2025.helper.PaginationHelper;
 import org.ieslluissimarro.rag.rag2daw2025.helper.SQLHelper;
+import org.ieslluissimarro.rag.rag2daw2025.model.dto.ChatList;
 import org.ieslluissimarro.rag.rag2daw2025.model.dto.FiltrosRequest;
+import org.ieslluissimarro.rag.rag2daw2025.model.dto.ListadoRespuesta;
+import org.ieslluissimarro.rag.rag2daw2025.model.dto.PaginaDto;
 import org.ieslluissimarro.rag.rag2daw2025.srv.FiltrosYEstadisticasService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,72 +35,86 @@ public class FiltrosYEstadisticasController {
     @Autowired
     private FiltrosYEstadisticasService service;
 
-
-    /* Ejemplo de json que recibe el endpoint filter
-        Los campos enviados serán los unicos en ser filtrados, por lo que solo envies los campos que quieres que se apliquen.
-        Si no quieres que se filtre por un campo no lo envies en el json ni pongas como valor null (excepto en las fechas donde si que aceptan null).
-        {
-            filterUser : "valor" ,
-            filterRango:YYYY-MM-DD HH:MM:SS|YYYY-MM-DD HH:MM:SS,  los campos pueden ser valores "null" y obtendrán valores por defecto
-            filterPregunta: "valor",
-            filterRespuesta: "valor",
-            filterFeedback: "valor", los valores normalmente son BIEN, NORMAL y MAL
-            filterValorado: true/false,
-            filterChunks: "valor"
-            
-        }
-
-     
+    /*
+     * Ejemplo de json que recibe el endpoint filter
+     * Los campos enviados serán los unicos en ser filtrados, por lo que solo envies
+     * los campos que quieres que se apliquen.
+     * Si no quieres que se filtre por un campo no lo envies en el json ni pongas
+     * como valor null (excepto en las fechas donde si que aceptan null).
+     * {
+     * filterUser : "valor" ,
+     * filterRango:YYYY-MM-DD HH:MM:SS|YYYY-MM-DD HH:MM:SS, los campos pueden ser
+     * valores "null" y obtendrán valores por defecto
+     * filterPregunta: "valor",
+     * filterRespuesta: "valor",
+     * filterFeedback: "valor", los valores normalmente son BIEN, NORMAL y MAL
+     * filterValorado: true/false,
+     * filterChunks: "valor"
+     * 
+     * }
+     * 
+     * 
      */
 
-
     @PostMapping("filter")
-    public ResponseEntity<?> getDatosByFiltroRequest(@RequestBody FiltrosRequest parametros) {
-        try {
+    public ResponseEntity<ListadoRespuesta<ChatList>> getDatosByFiltroRequest(@RequestBody FiltrosRequest parametros,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "50") int size,
+            @RequestParam(defaultValue = "idChat,asc") String[] sort) {
 
-            String query = SQLHelper.builderSentencias(JsonToMapHelper.converter(parametros));
+        Pageable paging = PaginationHelper.createPageable(page, size, sort);
 
-            return ResponseEntity.ok().body(service.executeQuery(query));
+        String query = SQLHelper.builderSentencias(JsonToMapHelper.converter(parametros));
 
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("ILLEGAL_ARGUMENT_EXCEPTION");
-        }
+        PaginaDto<ChatList> paginaChatInfo = service.executePaginatedQuery(query, paging);
 
-    }
-    
-/* 
-    @PostMapping("chats/filter")
-    public ResponseEntity<?> getDatosFiltradosChat(@RequestBody FiltrosRequest parametros) {
-        try {
+        ListadoRespuesta<ChatList> respuesta = new ListadoRespuesta<>(
+                paginaChatInfo.getNumber(),
+                paginaChatInfo.getSize(),
+                paginaChatInfo.getTotalElements(),
+                paginaChatInfo.getTotalPages(),
+                paginaChatInfo.getContent());
 
-            String query = SQLHelper.builderSentencias("chats", JsonToMapHelper.converter(parametros));
-
-            return ResponseEntity.ok().body(service.executeQuery(query));
-
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("ILLEGAL_ARGUMENT_EXCEPTION");
-        }
+        return ResponseEntity.ok().body(respuesta);
 
     }
-    
 
-    @GetMapping("preguntas/filter")
-    public ResponseEntity<?> getDatosFiltradosPreguntas(@RequestParam Map<String,String> parametros) {
-        try {
-            String query = SQLHelper.builderSentencias("preguntas", parametros);
-
-            return ResponseEntity.ok().body(service.executeQuery(query));
-
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("ILLEGAL_ARGUMENT_EXCEPTION");
-        }
-
-    }
-*/
+    /*
+     * @PostMapping("chats/filter")
+     * public ResponseEntity<?> getDatosFiltradosChat(@RequestBody FiltrosRequest
+     * parametros) {
+     * try {
+     * 
+     * String query = SQLHelper.builderSentencias("chats",
+     * JsonToMapHelper.converter(parametros));
+     * 
+     * return ResponseEntity.ok().body(service.executeQuery(query));
+     * 
+     * } catch (IllegalArgumentException e) {
+     * throw new IllegalArgumentException("ILLEGAL_ARGUMENT_EXCEPTION");
+     * }
+     * 
+     * }
+     * 
+     * 
+     * @GetMapping("preguntas/filter")
+     * public ResponseEntity<?> getDatosFiltradosPreguntas(@RequestParam
+     * Map<String,String> parametros) {
+     * try {
+     * String query = SQLHelper.builderSentencias("preguntas", parametros);
+     * 
+     * return ResponseEntity.ok().body(service.executeQuery(query));
+     * 
+     * } catch (IllegalArgumentException e) {
+     * throw new IllegalArgumentException("ILLEGAL_ARGUMENT_EXCEPTION");
+     * }
+     * 
+     * }
+     */
     @GetMapping("contexto/popular")
     public ResponseEntity<?> getListaContextos() {
         try {
-            String query = SQLHelper.selectContextoCountGrouped("chats" );
+            String query = SQLHelper.selectContextoCountGrouped("chats");
 
             return ResponseEntity.ok().body(service.executeQuery(query));
 
@@ -101,7 +123,6 @@ public class FiltrosYEstadisticasController {
         }
 
     }
-
 
     @GetMapping("getListUsuarios")
     public ResponseEntity<?> getListUsuarios() {
