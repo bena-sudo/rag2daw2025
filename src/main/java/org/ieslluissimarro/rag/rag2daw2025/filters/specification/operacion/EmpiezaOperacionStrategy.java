7be90f6e9315987318b2cd8d3946ead00eb82882
EdgarTormoJornet1 <edgtorjor@alu.edu.gva.es>
2025-02-16 18:37:21 +0100

    Filtro empieza por

---
+++ src/main/java/org/ieslluissimarro/rag/rag2daw2025/filters/specification/operacion/EmpiezaOperacionStrategy.java
@@ -0,0 +1,23 @@
+package org.ieslluissimarro.rag.rag2daw2025.filters.specification.operacion;
+import org.ieslluissimarro.rag.rag2daw2025.filters.model.FiltroBusqueda;
+import org.ieslluissimarro.rag.rag2daw2025.filters.model.TipoOperacionBusqueda;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+
+public class EmpiezaOperacionStrategy implements OperacionBusquedaStrategy {
+    @Override
+     public Predicate crearPredicado(
+        Root<?> root, 
+        CriteriaBuilder criteriaBuilder, 
+        FiltroBusqueda filtro
+    ) {
+        return criteriaBuilder.like(
+            root.get(filtro.getAtributo()), filtro.getValor() + "%");
+    }
+
+    @Override
+    public boolean soportaOperacion(TipoOperacionBusqueda operacion) {
+        return operacion == TipoOperacionBusqueda.EMPIEZA;
+    }
+}
