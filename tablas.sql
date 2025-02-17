805b11927bdf698d7b06558976bf0b665a9dee36
matecijas <matecijas@gmail.com>
2025-02-17 19:36:55 +0100

    ahora si

--- tablas.sql
+++ tablas.sql
@@ -3,6 +3,9 @@ DROP TABLE IF EXISTS documentchunks;
 DROP TABLE IF EXISTS preguntas;
 DROP TABLE IF EXISTS chats;
 
+
+
+-- Tablas de calidad
 CREATE TABLE chats (
     id_chat BIGSERIAL PRIMARY KEY,
     "user" VARCHAR NOT NULL,
@@ -21,14 +24,65 @@ CREATE TABLE preguntas (
     id_chat BIGINT NOT NULL REFERENCES chats(id_chat) ON DELETE CASCADE
 );
 
-CREATE TABLE documentchunks (
-    id_documentchunk BIGSERIAL PRIMARY KEY,
-    id_doc_source BIGINT NOT NULL,
-    contenido TEXT DEFAULT ' '
+--Tablas de documentacion
+
+
+
+CREATE TABLE documentos (
+    id SERIAL PRIMARY KEY,
+    id_doc_rag INT,
+    id_usuario BIGSERIAL NOT NULL,
+    nombre_fichero VARCHAR(255) NOT NULL,
+    comentario TEXT,
+    base64_documento TEXT,
+    extension_documento VARCHAR(5),
+    content_type_documento VARCHAR(100),
+    tipo_documento VARCHAR(50),
+    estado_documento VARCHAR(20), -- "pendiente", "aprobado", "denegado"
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_revision TIMESTAMP
 );
+
+CREATE TABLE documentos_chunks (
+    id SERIAL PRIMARY KEY,
+    id_documento SERIAL NOT NULL REFERENCES documentos(id) ON DELETE CASCADE,
+    id_doc_rag INT,
+    chunk_order INTEGER NOT NULL,
+    chunk_text TEXT NOT NULL,
+    chunked_by BIGINT,
+    estado VARCHAR(20) DEFAULT 'PENDIENTE',
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_modificacion TIMESTAMP
+);
+
+
+CREATE TABLE etiquetas (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE documentos_etiquetas (
+    id_documento SERIAL NOT NULL REFERENCES documentos(id) ON DELETE CASCADE,
+    id_etiqueta INT NOT NULL REFERENCES etiquetas(id) ON DELETE CASCADE,
+    PRIMARY KEY (id_documento, id_etiqueta)
+);
+
+CREATE TABLE estadistica_documental (
+    id SERIAL PRIMARY KEY,
+    id_documento SERIAL REFERENCES documentos(id) ON DELETE SET NULL,
+    id_chunk SERIAL REFERENCES documentos_chunks(id) ON DELETE SET NULL,
+    tiempo_revision INT, -- Tiempo total en segundos
+    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    usuario BIGINT, -- Usuario que realizó la revisión
+    estado_final VARCHAR(20) -- Estado final después de la revisión
+);
+
+--Tablas mixtas
+
+--Documental + qualitat
 
 CREATE TABLE pregunta_documentchunk (
     id_pregunta BIGINT NOT NULL REFERENCES preguntas(id_pregunta) ON DELETE CASCADE,
-    id_documentchunk BIGINT NOT NULL REFERENCES documentchunks(id_documentchunk) ON DELETE CASCADE,
+    id_documentchunk BIGINT NOT NULL REFERENCES documentos_chunks(id) ON DELETE CASCADE,
     PRIMARY KEY (id_pregunta, id_documentchunk)
 );
