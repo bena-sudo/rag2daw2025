7e05b6923855c37edfc9511abc9262fd636e4e2f
matecijas <matecijas@gmail.com>
2025-02-19 17:12:46 +0100

    a

--- src/main/java/org/ieslluissimarro/rag/rag2daw2025/controller/ChatController.java
+++ src/main/java/org/ieslluissimarro/rag/rag2daw2025/controller/ChatController.java
@@ -56,42 +56,40 @@ public class ChatController {
 
     @Operation(summary = "Crea un nuevo registro de tipo chat en el sistema.")
     @ApiResponses(value = {
-	    @ApiResponse(responseCode = "200", description = "OK: Chat encontrado con éxito", content = {
-		    @Content(mediaType = "application/json", schema = @Schema(implementation = ChatEdit.class)) }),
-	    @ApiResponse(responseCode = "400", description = "Bad Request: Error de validación en el ID proporcionado (errorCode='ID_FORMAT_INVALID')", content = {
-		    @Content(mediaType = "application/json", schema = @Schema(implementation = CustomErrorResponse.class)) }),
-	    @ApiResponse(responseCode = "404", description = "Not Found: No se encontró el Chat con el ID proporcionado (errorCode='CHAT_NOT_FOUND')", content = {
-		    @Content(mediaType = "application/json", schema = @Schema(implementation = CustomErrorResponse.class)) })
+            @ApiResponse(responseCode = "200", description = "OK: Chat encontrado con éxito", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = ChatEdit.class)) }),
+            @ApiResponse(responseCode = "400", description = "Bad Request: Error de validación en el ID proporcionado (errorCode='ID_FORMAT_INVALID')", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = CustomErrorResponse.class)) }),
+            @ApiResponse(responseCode = "404", description = "Not Found: No se encontró el Chat con el ID proporcionado (errorCode='CHAT_NOT_FOUND')", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = CustomErrorResponse.class)) })
     })
     @PostMapping("createChat")
     public ResponseEntity<ChatInfo> createChat(@Valid @RequestBody ChatList chatEdit, BindingResult bindingResult) {
 
-	BindingResultHelper.validationBindingResult(bindingResult, "CHAT_CREATE_VALIDATION");
+        BindingResultHelper.validationBindingResult(bindingResult, "CHAT_CREATE_VALIDATION");
 
-	return ResponseEntity.status(HttpStatus.CREATED).body(chatService.create(chatEdit));
+        return ResponseEntity.status(HttpStatus.CREATED).body(chatService.create(chatEdit));
     }
 
     @GetMapping("returnChats")
     public ResponseEntity<ListadoRespuesta<ChatList>> devuelveListaDeChatList(
-		@RequestParam(required = false) String[] filter,
-    	@RequestParam(defaultValue = "0") int page,
-    	@RequestParam(defaultValue = "50") int size,
-    	@RequestParam(defaultValue = "idChat,asc") String[] sort
-	) throws FiltroException {
+            @RequestParam(required = false) String[] filter,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "50") int size,
+            @RequestParam(defaultValue = "idChat,asc") String[] sort) throws FiltroException {
 
-		List<FiltroBusqueda> listarFiltros = FiltroBusquedaHelper.createFiltroBusqueda(filter);
+        List<FiltroBusqueda> listarFiltros = FiltroBusquedaHelper.createFiltroBusqueda(filter);
 
         Pageable paging = PaginationHelper.createPageable(page, size, sort);
 
-		PaginaDto<ChatList> paginaChatInfo = chatService.findAllPageChatInfo(listarFiltros, paging);
+        PaginaDto<ChatList> paginaChatInfo = chatService.findAllPageChatInfo(listarFiltros, paging);
 
         ListadoRespuesta<ChatList> respuesta = new ListadoRespuesta<>(
                 paginaChatInfo.getNumber(),
                 paginaChatInfo.getSize(),
                 paginaChatInfo.getTotalElements(),
                 paginaChatInfo.getTotalPages(),
-                paginaChatInfo.getContent()
-        );
+                paginaChatInfo.getContent());
 
         return ResponseEntity.ok(respuesta);
     }
@@ -104,14 +102,10 @@ public class ChatController {
     @Operation(summary = "Devuelve un listado de PreguntaInfo dado un idChat.")
     @GetMapping("returnPreguntasByIdChat")
     public ResponseEntity<List<PreguntaInfo>> devuelvePreguntasByIdChat(@RequestParam Long idChat) {
-        
+
         return ResponseEntity.ok(chatService.getChatPreguntasByIdChat(idChat));
     }
 
-
-
-
-    
     @DeleteMapping("deleteChat")
     public ResponseEntity<Void> delete(@RequestParam Long idChat) {
         chatService.delete(idChat);
@@ -120,24 +114,24 @@ public class ChatController {
 
     @Operation(summary = "Actualiza los datos de un chat existente en el sistema.")
     @ApiResponses(value = {
-	    @ApiResponse(responseCode = "200", description = "OK: Chat actualizado con éxito", content = {
-		    @Content(mediaType = "application/json", schema = @Schema(implementation = PreguntaEdit.class)) }),
-	    @ApiResponse(responseCode = "400", description = "Bad Request: Errores de validación en los datos proporcionados (errorCode='CHAT_UPDATE_VALIDATION')", content = {
-		    @Content(mediaType = "application/json", schema = @Schema(implementation = BindingResultErrorsResponse.class)) }),
-	    @ApiResponse(responseCode = "400", description = "Bad Request: Errores de validación en el ID proporcionado (errorCodes='ID_FORMAT_INVALID','ID_CHAT_MISMATCH')", content = {
-		    @Content(mediaType = "application/json", schema = @Schema(implementation = CustomErrorResponse.class)) }),
-	    @ApiResponse(responseCode = "404", description = "Not Found: No se encontró el Chat con el ID proporcionado (errorCode='CHAT_NOT_FOUND_FOR_UPDATE')", content = {
-		    @Content(mediaType = "application/json", schema = @Schema(implementation = CustomErrorResponse.class)) }),
-	    @ApiResponse(responseCode = "409", description = "Conflict: Error al intentar actualizar un 'Chat' (errorCodes: 'FOREIGN_KEY_VIOLATION', 'UNIQUE_CONSTRAINT_VIOLATION', 'DATA_INTEGRITY_VIOLATION')", content = {
-		    @Content(mediaType = "application/json", schema = @Schema(implementation = CustomErrorResponse.class)) })
+            @ApiResponse(responseCode = "200", description = "OK: Chat actualizado con éxito", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = PreguntaEdit.class)) }),
+            @ApiResponse(responseCode = "400", description = "Bad Request: Errores de validación en los datos proporcionados (errorCode='CHAT_UPDATE_VALIDATION')", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = BindingResultErrorsResponse.class)) }),
+            @ApiResponse(responseCode = "400", description = "Bad Request: Errores de validación en el ID proporcionado (errorCodes='ID_FORMAT_INVALID','ID_CHAT_MISMATCH')", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = CustomErrorResponse.class)) }),
+            @ApiResponse(responseCode = "404", description = "Not Found: No se encontró el Chat con el ID proporcionado (errorCode='CHAT_NOT_FOUND_FOR_UPDATE')", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = CustomErrorResponse.class)) }),
+            @ApiResponse(responseCode = "409", description = "Conflict: Error al intentar actualizar un 'Chat' (errorCodes: 'FOREIGN_KEY_VIOLATION', 'UNIQUE_CONSTRAINT_VIOLATION', 'DATA_INTEGRITY_VIOLATION')", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = CustomErrorResponse.class)) })
     })
     @PutMapping("/updateChat/{id}")
     public ResponseEntity<ChatEdit> update(@PathVariable Long id, @Valid @RequestBody ChatEdit chatEdit,
-	    BindingResult bindingResult) {
+            BindingResult bindingResult) {
 
-	BindingResultHelper.validationBindingResult(bindingResult, "CHAT_UPDATE_VALIDATION");
+        BindingResultHelper.validationBindingResult(bindingResult, "CHAT_UPDATE_VALIDATION");
 
-	return ResponseEntity.ok(chatService.update(id, chatEdit));
+        return ResponseEntity.ok(chatService.update(id, chatEdit));
     }
 
     @GetMapping(value = "/reply/{idPregunta}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
@@ -145,13 +139,14 @@ public class ChatController {
         String respuesta = "Simulación de respuesta. Esto es una simulación para comprobar si realmente funciona el flujo que hemos incorporado en el backend y el SSE que hay en el frontend.";
 
         List<Character> caracteres = respuesta.chars()
-            .mapToObj(c -> (char) c)
-            .collect(Collectors.toList());
-
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.toList());
         // Agregar el carácter especial al final
-            caracteres.add('\u0003');
+        caracteres.add('\u0003');
 
         return Flux.fromIterable(caracteres)
-            .delayElements(Duration.ofMillis(50));
+                .delayElements(Duration.ofMillis(50));
+
     }
+
 }
