2896246d986bf5b05ac1c66f510a34d9aad58e40
itsxHrisi <hristiyatg@gmail.com>
2025-02-21 02:39:07 +0100

    Ultimo intento v2

--- src/main/java/org/ieslluissimarro/rag/rag2daw2025/config/WebConfig.java
+++ src/main/java/org/ieslluissimarro/rag/rag2daw2025/config/WebConfig.java
@@ -1,13 +1,23 @@
 package org.ieslluissimarro.rag.rag2daw2025.config;
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://13.73.226.200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
