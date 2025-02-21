e1679b183f407213ebc6fe0e5ebf89b5461a351c
itsxHrisi <hristiyatg@gmail.com>
2025-02-21 01:12:13 +0100

    Cors try 8

--- src/main/java/org/ieslluissimarro/rag/rag2daw2025/Rag2daw2025Application.java
+++ src/main/java/org/ieslluissimarro/rag/rag2daw2025/Rag2daw2025Application.java
@@ -2,7 +2,10 @@ package org.ieslluissimarro.rag.rag2daw2025;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableScheduling
@@ -12,4 +15,15 @@ public class Rag2daw2025Application {
 		SpringApplication.run(Rag2daw2025Application.class, args);
 	}
 
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // Permitir cualquier origen
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
 }
